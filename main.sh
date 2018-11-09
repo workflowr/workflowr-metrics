7de6f440da9a2294c8cc35c18e8d6ae271d5825c
John Blischak <jdblischak@gmail.com>
2018-11-09 14:05:37 -0500

    Combine new views data with existing data.

--- main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Rscript scripts/cranlogs.R > data/cranlogs.txt
-Rscript scripts/github-views.R > data/github-views.txt
--- scripts/github-views.R
+++ scripts/github-views.R
@@ -2,6 +2,8 @@
 
 # Get number of views of workflowr GitHub repository
 #
+# Requires environment variable GITHUB_PAT with GitHub token
+#
 # https://help.github.com/articles/about-repository-graphs/#traffic
 # https://github.com/jdblischak/workflowr/graphs/traffic
 # https://help.github.com/articles/viewing-traffic-to-a-repository/
@@ -11,6 +13,12 @@ suppressPackageStartupMessages(library("gh"))
 suppressPackageStartupMessages(library("lubridate"))
 suppressPackageStartupMessages(library("purrr"))
 
+fname <- commandArgs(trailingOnly = TRUE)[1]
+stopifnot(file.exists(fname))
+
+datafile <- read.delim(fname, stringsAsFactors = FALSE)
+datafile$date <- as_date(datafile$date)
+
 views <- gh("/repos/:owner/:repo/traffic/views",
             owner = "jdblischak", repo = "workflowr")
 views <- views$views
@@ -18,5 +26,9 @@ views <- map_dfr(views, function(x) as.data.frame(x, stringsAsFactors = FALSE))
 views$timestamp <- as_date(views$timestamp)
 colnames(views)[1] <- "date"
 
-write.table(views, file = stdout(), quote = FALSE, sep = "\t",
+# Combine
+date_min <- min(views$date)
+output <- rbind(datafile[datafile$date < date_min, ], views)
+
+write.table(output, file = fname, quote = FALSE, sep = "\t",
             row.names = FALSE)
