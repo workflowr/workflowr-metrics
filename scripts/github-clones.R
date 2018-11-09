010df3e3c6ff7ca7b11072a6e75f784ed1b37b85
John Blischak <jdblischak@gmail.com>
2018-11-09 14:26:05 -0500

    Save GitHub clones.

---
+++ scripts/github-clones.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+# Get number of clones of workflowr GitHub repository for last 14 days
+#
+# Requires environment variable GITHUB_PAT with GitHub token
+#
+# https://developer.github.com/v3/repos/traffic/#clones
+
+suppressPackageStartupMessages(library("gh"))
+suppressPackageStartupMessages(library("lubridate"))
+suppressPackageStartupMessages(library("purrr"))
+
+fname <- commandArgs(trailingOnly = TRUE)[1]
+# fname <- "data/github-clones.txt"
+stopifnot(file.exists(fname))
+
+datafile <- read.delim(fname, stringsAsFactors = FALSE)
+datafile$date <- as_date(datafile$date)
+
+clones <- gh("/repos/:owner/:repo/traffic/clones",
+            owner = "jdblischak", repo = "workflowr")
+clones <- clones$clones
+clones <- map_dfr(clones, function(x) as.data.frame(x, stringsAsFactors = FALSE))
+clones$timestamp <- as_date(clones$timestamp)
+colnames(clones)[1] <- "date"
+
+# Combine
+date_min <- min(clones$date)
+output <- rbind(datafile[datafile$date < date_min, ], clones)
+
+write.table(output, file = fname, quote = FALSE, sep = "\t", row.names = FALSE)
