3127e493fbff77003bec98e3902de5ae4e4dfe4b
John Blischak <jdblischak@gmail.com>
2018-11-12 15:32:57 -0500

    Use .limit arg of gh() to download all stars with one function call.

--- scripts/github-stars.R
+++ scripts/github-stars.R
@@ -13,20 +13,10 @@ fname <- commandArgs(trailingOnly = TRUE)[1]
 # fname <- "data/github-stars.txt"
 stopifnot(file.exists(fname))
 
-stars <- list()
-# per_page maxed out at 100
-page <- 1
-while (TRUE) {
-  g <- gh("/repos/:owner/:repo/stargazers",
-          owner = "jdblischak", repo = "workflowr",
-          .send_headers = c(Accept = "application/vnd.github.v3.star+json"),
-          per_page = 100, page = page)
-  if (length(g) == 1 && g == "") break
-  stars <- c(stars, g)
-  if (length(g) < 100 || page > 20) break
-  page <- page + 1
-  Sys.sleep(0.25)
-}
+stars <- gh("/repos/:owner/:repo/stargazers",
+        owner = "jdblischak", repo = "workflowr",
+        .send_headers = c(Accept = "application/vnd.github.v3.star+json"),
+        per_page = 100, .limit = Inf)
 
 starred_at <- Map(function(x) x$starred_at, stars)
 starred_at <- unlist(starred_at)
